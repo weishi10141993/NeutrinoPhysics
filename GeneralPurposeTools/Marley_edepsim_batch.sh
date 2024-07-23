e9ff537761c0748261a00019f8d5dceb80687e06
Wei Shi <wei.shi.1@stonybrook.edu>
2024-07-23 14:11:50 -0400

    add batch prod

---
+++ GeneralPurposeTools/Marley_edepsim_batch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Running Marley edepsim chain"
+for (( k = 91; k < 100; ++k )); do
+  ienergy=$(echo "scale=1; $k*0.5" | bc)
+  echo "running $ienergy MeV"
+  ./MarleyGen_MonoE_Flux_Edepsim.sh ${ienergy} 1000
+  mv edep.${ienergy}.MeV.nue.Evt.1000.root edep_nue_${ienergy}MeV_1kevts.root
+  mv edep_nue_${ienergy}MeV_1kevts.root /pnfs/dune/persistent/users/weishi/FD3/LArBath/Marley_Edepsim_noSecondaryDeposit/nue
+done
+echo "Done."
